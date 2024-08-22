2f16313728fab712d9420af4f722a4bbbecc1007
nishanttomar21 <nishanttomar21@gmail.com>
2024-08-23 00:07:29 +0530

    Implemented Fetch Type and Fetch Mode

--- src/main/java/org/example/fakeStore/FakeStoreApplication.java
+++ src/main/java/org/example/fakeStore/FakeStoreApplication.java
@@ -1,3 +1,14 @@
+// This is the main class of the application. It is the entry point of the Spring Boot application.
+// The @SpringBootApplication annotation is used to mark the class as a Spring Boot application. It is a convenience annotation that combines @Configuration, @EnableAutoConfiguration, and @ComponentScan annotations.
+// SpringApplication.run is a static method that starts the Spring Boot application. It takes the main class and command-line arguments as arguments.
+// Code Flow: The main method starts the Spring Boot application, which in turn starts the embedded Tomcat server and initializes the application context. The application context is responsible for managing the beans and dependencies in the application.
+// The application context scans the packages for components (controllers, services, repositories) annotated with @Component, @Service, @Repository, etc., and creates instances of these beans.
+// The application context also reads the application.properties or application.yml file for configuration properties and initializes the beans accordingly.
+// The application context starts the embedded Tomcat server and listens for incoming HTTP requests. When a request is received, it routes it to the appropriate controller method based on the request mapping annotations.
+// The controller method processes the request, interacts with the service layer to perform business logic, and returns the response to the client.
+// The response is sent back to the client, and the application context manages the lifecycle of the beans until the application is shut down.
+// Dispatch Servlet --> Advices --> Controller --> Service --> Repository --> ORM(JPA, Hibernate, JDBC) --> Database
+
 package org.example.fakeStore;
 
 import org.springframework.boot.SpringApplication;
--- src/main/java/org/example/fakeStore/models/Category.java
+++ src/main/java/org/example/fakeStore/models/Category.java
@@ -9,15 +9,25 @@
 //      CascadeType.REFRESH - It ensures that when a parent entity is refreshed (reloaded/updated from the database), all related child entities marked with CascadeType.REFRESH are also refreshed automatically.
 //      CascadeType.ALL - It ensures that all the above cascade operations are applied to the parent entity.
 //      CascadeType.NONE - It ensures that no cascade operations are applied to the parent entity. Default Cascade Type is NONE (cascade = {}).
+// FetchType - (when will an attribute be fetched) It is used to specify the fetching strategy to retrieve the related entities. Used when you have a relation between two entities. There are two types of FetchType:
+//      FetchType.LAZY - It fetches the related entities lazily, i.e., only when they are requested. When you try to get object of class, the value of that attribute will be fetched when accessed. It is the default fetch type for @OneToMany and @ManyToMany relationships. By default Collections(List, Set, Map) are lazy fetchType, collections leads to calling joins in SQL. It means that when you load an entity, the collection is not loaded. It is loaded only when you try to access it. Mostly for Collections you use LAZY and only use EAGER when the no. of items are less and those items are almost needed together along with the object of the parent class.
+//      FetchType.EAGER - It fetches the related entities eagerly, i.e., at the time of fetching the parent entity. When you try to get object of class, the value of all the attributes will be fetched. It is the default fetch type for @OneToOne and @ManyToOne relationships.
+//      FetchType.LAZY is more efficient than FetchType.EAGER as it loads the related entities only when they are needed. However, FetchType.LAZY can lead to LazyInitializationException if the related entities are accessed outside the transaction boundary.
+//      FetchType.EAGER can lead to performance issues as it fetches all the related entities even if they are not needed. It is recommended to use FetchType.LAZY for @OneToMany and @ManyToMany relationships to avoid performance issues.
+// FetchMode - (how will an attribute be fetched) It is used to specify the fetching mode to retrieve the related entities. Used when you have a relation between two entities. There are three types of FetchMode:
+//      FetchMode.SELECT - It fetches the related entities using a separate SELECT query. It is the default fetch mode for @OneToMany and @ManyToMany relationships. It means that when you load an entity, the collection is not loaded. It is loaded only when you try to access it.
+//      FetchMode.SUBSELECT - It fetches the related entities using a separate SELECT query with a subquery statement. It is used to fetch the related entities in a single query. It is more efficient than FetchMode.SELECT as it reduces the number of queries executed. It is recommended to use FetchMode.SUBSELECT for @OneToMany and @ManyToMany relationships to improve performance.
+//      FetchMode.JOIN - It fetches the related entities using a JOIN query. It is the default fetch mode for @OneToOne and @ManyToOne relationships. It means that when you load an entity, the collection is loaded along with it. It is loaded even if you do not access it.
+// FetchMode.SELECT is more efficient than FetchMode.JOIN as it loads the related entities using a separate SELECT query only when they are needed. However, FetchMode.SELECT can lead to N+1 query issues if the related entities are accessed in a loop. FetchMode.JOIN fetches all the related entities using a JOIN query even if they are not needed. It is recommended to use FetchMode.SELECT for @OneToMany and @ManyToMany relationships to avoid N+1 query issues.
+// FetchType.LAZY and FetchMode.SELECT are used together to fetch the related entities lazily using a separate SELECT query only when they are needed. FetchType.EAGER and FetchMode.JOIN are used together to fetch the related entities eagerly using a JOIN query even if they are not needed.
 
 package org.example.fakeStore.models;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import java.util.List;
 
@@ -25,13 +35,19 @@ import java.util.List;
 @Setter
 @Entity
 public class Category extends BaseModel {
+    @Column(nullable = false, unique = true)  // Column annotation is used to specify the column details of the entity. In this case, we are specifying that the name column is not nullable (validation, cannot be empty) and unique.
     private String name;
+
+    @Basic(fetch = FetchType.LAZY)
     private String description;
 
-    @OneToMany
+    @OneToMany(fetch = FetchType.EAGER)
+    @Fetch(FetchMode.SELECT)
     private List<Product> featuredProducts;
 
-    @OneToMany(mappedBy = "category" )   // category attribute of Product is representing this relation; [Important] If same relation is represented from both the classes, Spring may end up representing the relation twice in the database. To avoid this, we need to tell Spring that they are the same relation. This can be done by using the mappedBy attribute in the @OneToMany annotation to tell that it has already been marked by someone else.
+    @OneToMany(mappedBy = "category")   // Collections by default FetchType = LAZY
+    @Fetch(FetchMode.JOIN)
+    // category attribute of Product is representing this relation; [Important] If same relation is represented from both the classes, Spring may end up representing the relation twice in the database. To avoid this, we need to tell Spring that they are the same relation. This can be done by using the mappedBy attribute in the @OneToMany annotation to tell that it has already been marked by someone else.
     // @OneToMany(mappedBy = "category", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})  // Example of cascade
     private List<Product> allProducts;
 
