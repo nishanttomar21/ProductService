94a435454f468dfed49750dd8d0c52dc9ce01ed9
nishanttomar21 <nishanttomar21@gmail.com>
2024-08-25 14:24:25 +0530

    Minor changes

--- src/main/java/org/example/fakeStore/ProductServiceApplication.java
+++ src/main/java/org/example/fakeStore/ProductServiceApplication.java
@@ -15,10 +15,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FakeStoreApplication {
+public class ProductServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FakeStoreApplication.class, args);
+		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
 }
